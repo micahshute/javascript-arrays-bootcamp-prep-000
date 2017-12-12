4eb18976397d88f23e7e1b70c7f37fff282809f9
Micah Shute <micah.shute@gmail.com>
2017-12-12 04:22:36 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+function addElementToEndOFArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element)
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+
